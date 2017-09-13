bd29d3e36cad236d305fe29faa34ba41188c60b9
Sune Nilausen <sune@cs2.dk>
2017-09-13 14:15:49 +0200

    New feature. Almost done with cv and footer

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  get 'pages/index'
-  get 'pages/dickbutt'
-  get 'pages/skills'
-  get 'pages/timeline'
+  get 'index', to: 'pages#index'
+  get 'portfolio', to: 'pages#portfolio'
+  get 'cv', to: 'pages#cv'
 
   root to: 'pages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
